03834013065b6795cfafd3cca3d1476b2c508c77
toniahurst <antonia.a.hurst@gmail.com>
2019-08-26 19:02:41 +0000

    L16. Added carrierwave. Things not working yet. Lots of test failures.

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ end
 gem 'simple_form'
 
 gem 'devise'
+
+gem 'carrierwave', '~> 2.0'
--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,10 @@
 class Gram < ApplicationRecord
+	mount_uploader :picture, PictureUploader
+	
 	validates :message, presence: true
+
+
+	
 
 	belongs_to :user
 end
