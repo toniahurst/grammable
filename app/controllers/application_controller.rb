b55ab48d69b7c0b5a490d01d3168bd0802366bc9
toniahurst <antonia.a.hurst@gmail.com>
2019-08-26 20:18:11 +0000

    L19. Still has problems.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,8 @@
-class ApplicationController < ApplicationRecord
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+class ApplicationController < ActionController::Base
 
+protect_from_forgery with: :exception
 
-  def render_not_found(status=:not_found)
+def render_not_found(status=:not_found)
     render plain: "#{status.to_s.titleize} :(", status: status
   end
-
 end
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -15,10 +15,12 @@ class GramsController < ApplicationController
 	end
 
 	def edit
-		@gram = Gram.find_by_id(params[:id])
-		return render_not_found if @gram.blank?
-		return render_not_found(:forbidden) if @gram.user != current_user
-	end
+  	@gram = Gram.find_by_id(params[:id])
+  		return render_not_found if @gram.blank?
+  	if @gram.user != current_user
+    render plain: 'Forbidden :(', status: :forbidden
+  end
+end
 
 	def create
 		@gram = current_user.grams.create(gram_params)
@@ -28,27 +30,29 @@ class GramsController < ApplicationController
 			render :new, status: :unprocessable_entity
 		end
 	end
-
-	def update
-		@gram = Gram.find_by_id(params[:id])
-		return render_not_found if @gram.blank?
-		return render_not_found(:forbidden) if @gram.user != current_user
-
-		@gram.update_attributes(gram_params)
-		if @gram.valid?
-			redirect_to root_path
-		else
-			return render :edit, status: :unprocessable_entity
-		end
-	end
+def update
+  @gram = Gram.find_by_id(params[:id])
+  return render_not_found if @gram.blank?
+  if @gram.user != current_user
+    return render plain: 'Forbidden :(', status: :forbidden
+  end
+  @gram.update_attributes(gram_params)
+  if @gram.valid?
+    redirect_to root_path
+  else
+    return render :edit, status: :unprocessable_entity
+  end
+end
 
 	def destroy
-		@gram = Gram.find_by_id(params[:id])
-		return render_not_found if @gram.blank?
-		return render_not_found(:forbidden) if @gram.user != current_user
-		@gram.destroy
-		redirect_to root_path
-	end
+  @gram = Gram.find_by_id(params[:id])
+  return render_not_found if @gram.blank?
+  if @gram.user != current_user
+    return render plain: 'Forbidden :(', status: :forbidden
+  end
+  @gram.destroy
+  redirect_to root_path
+end
 
 	 private
 
